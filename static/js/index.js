288f25bb2d5263061d6b1b40de8d1e89349aa5a5
HadiZayer <hmzayer@hotmail.com>
2023-04-06 18:38:11 -0400

    twentytwenty check image loaded

--- static/js/index.js
+++ static/js/index.js
@@ -268,8 +268,21 @@ images.forEach(function(image) {
   };
 });
 
-$(function(){
-  console.log("num of slider images " + images.length);
+// $(function(){
+//   console.log("num of slider images " + images.length);
+//   $(".compare").twentytwenty({
+//     default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+//     orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+//     before_label: 'Input', // Set a custom before label
+//     after_label: 'Output', // Set a custom after label
+//     no_overlay: true, //Do not show the overlay with before and after
+//     move_slider_on_hover: true, // Move slider on mouse hover?
+//     move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+//     click_to_move: true // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+//   });
+// });
+
+$('.compare').imagesLoaded( function() {
   $(".compare").twentytwenty({
     default_offset_pct: 0.5, // How much of the before image is visible when the page loads
     orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
@@ -280,11 +293,11 @@ $(function(){
     move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
     click_to_move: true // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
   });
-
+  console.log("images loaded");
   document.getElementById('book_deblurring').style.display = 'none';
   document.getElementById('microkitchen_refocusing').style.display = 'none';
 });
 
-$(window).load(function() {
-  $(window).trigger("resize.twentytwenty");
-});
+// $(window).load(function() {
+//   $(window).trigger("resize.twentytwenty");
+// });
