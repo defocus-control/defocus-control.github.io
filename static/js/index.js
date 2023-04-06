7dec4de362a06e01d4532442422fc951e3b9c3bf
HadiZayer <hmzayer@hotmail.com>
2023-04-06 19:32:22 -0400

    another patch

--- static/js/index.js
+++ static/js/index.js
@@ -285,17 +285,19 @@ $('.compare').imagesLoaded(function() {
 });
 
 setTimeout(function() {
-  $(".compare").twentytwenty({
-    default_offset_pct: 0.5, // How much of the before image is visible when the page loads
-    orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
-    before_label: 'Input', // Set a custom before label
-    after_label: 'Output', // Set a custom after label
-    no_overlay: true, //Do not show the overlay with before and after
-    move_slider_on_hover: true, // Move slider on mouse hover?
-    move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
-    click_to_move: true // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
-  }); console.log("loaded after tiemout");
-  }, 500);
+  // $(".compare").twentytwenty({
+  //   default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+  //   orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+  //   before_label: 'Input', // Set a custom before label
+  //   after_label: 'Output', // Set a custom after label
+  //   no_overlay: true, //Do not show the overlay with before and after
+  //   move_slider_on_hover: true, // Move slider on mouse hover?
+  //   move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+  //   click_to_move: true // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+  // });
+  window.dispatchEvent(new Event('resize')); 
+  console.log("loaded after tiemout");
+  }, 2000);
 
 // $(window).load(function() {
 //   $(window).trigger("resize.twentytwenty");
